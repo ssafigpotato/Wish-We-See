4d83b60405b71dec107aaf6e022023cf38a0575b
jwun95 <jwun95@naver.com>
2024-09-24 14:33:42 +0900

    Chore: Service Worker map 파일 gitignore & gitignore reset

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2ab19099bba426dd2c58b5cac595b010139fe086a48828ffa80fd227a153147
-size 289
+oid sha256:4bd9b2fe0d381fc800dc1b102e9f06bff736fd760df180722995aa50f8bd22ec
+size 327
