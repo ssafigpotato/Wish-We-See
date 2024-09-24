443d3e222261ac2ebec83e9b4dd23add049020ec
inception010203 <inception010203@gmail.com>
2024-09-24 16:56:57 +0900

    Fix: MR 테스트(S11P21A202-413)

--- client/src/components/atoms/Button/Button.tsx
+++ client/src/components/atoms/Button/Button.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e9bca7208daf2aabf523f88326e560688cb2b867f8e625ed960bd5428878908
-size 28
+oid sha256:9c66855b0b48eec1788f89ec082bf8c56af51a1f56fb2fb185956c6779a60d8c
+size 30
