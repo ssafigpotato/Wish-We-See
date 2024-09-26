57c846c76ca26ce8b1b3f9939afb7a6ecb98535c
jwun95 <jwun95@naver.com>
2024-09-26 10:15:54 +0900

    Design: font, color css 파일 작성
    Former-commit-id: bdbf5fd69098b2814fc344a7d1c3f42f15b4b8e1

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,3 @@
-export default function Home() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4b4d041948abc47e3ec0fe7ea20c7b040547ad4cb8661b8045c4adca74456b74
+size 156
