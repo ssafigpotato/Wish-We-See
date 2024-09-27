8ec1c7813695926ea2397b716712058abaf06efe
inception010203 <inception010203@gmail.com>
2024-09-27 17:17:04 +0900

    Feat: 가속도 센서 활성화 여부 상태 코드 추가(S11P21A202-594)
    Former-commit-id: 78d159838cd2024075ea9f58818b64eadb075ee5

--- client/components/sensor/AccelSeonsor.tsx
+++ client/components/sensor/AccelSeonsor.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06009691fa20065402c1519aa723c91720cfe033adc61803d6b5c229035910b3
-size 2818
+oid sha256:c4bbd6feda1c3155706f18959bea88e975da3a950b9b010614cdad81a7a7b317
+size 3255
