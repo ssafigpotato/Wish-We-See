cb9c688014d10701ca7685624becf2c3758d24f2
LeeSpect <wewqwew153@gmail.com>
2024-09-27 16:17:37 +0900

    Feat: UserService에 logout 메서드 구현(S11P21A202-492)
    Former-commit-id: 7ad9e851a55c83ccf0f918b25de1ba20636c4b10

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserService.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:178a32b403ccf9ad0a64c427f79b9f6acf9443b1a9e86c36ea389be16c33d4c7
-size 200
+oid sha256:040837b00a5b03a4cf3bd5c9c02198334f9e70c5c0550dd33198859906ba5328
+size 217
