749e7c9b0f1c61ac0bb4587e01a1512540865752
LeeSpect <wewqwew153@gmail.com>
2024-09-27 15:56:50 +0900

    Feat: UserService에 signIn 메서드 구현(S11P21A202-492)
    Former-commit-id: d0e04c7a8912a63793ef1efaa3001c1a4067bc61

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
@@ -1,17 +1,3 @@
-package com.ssafy.missing_back.domain.users.service;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.ssafy.missing_back.domain.users.repository.UserRepository;
-
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class UserServiceImpl implements UserService{
-
-	private final UserRepository userRepository;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:890c1badff2e09837cc047c5fb354ddf29469c2da74e3ee1baaeedecf933c51f
+size 1764
