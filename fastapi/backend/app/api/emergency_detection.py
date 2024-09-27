aceab112ed0a031b895cb1d40dcc27df4284ae06
hybrid417 <hybrid417@naver.com>
2024-09-27 13:00:47 +0900

    Docs: 프로젝트 구조 변경

---
+++ fastapi/backend/app/api/emergency_detection.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1224e2ef38cefcead30c563b0d760d1fc1627cd166700bbbaec02195168dbbc8
+size 736
---
+++ fastapi/backend/app/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:54bf34dcfd10c7f73374928f4a89c768b39eac687c5b663950a3c58fe776fa81
+size 209
