b87493862f63b5968e36e8a83613db4aa5fb728e
inception010203 <inception010203@gmail.com>
2024-09-27 16:39:03 +0900

    Feat: 가속도 센서 테스트 코드 작성(S11P21A202-594)
    Former-commit-id: c4337782fd2700893e039da310c10d09b5290f1c

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07456977a324d8732d1e16c14da6842e257eb2e85d09ab88bb632b9d348d76cb
-size 2370
+oid sha256:a3f4b6a661574eaa58ac5002a401a9f6e9819257e75ca46a74149028d56a9e5f
+size 2680
---
+++ client/components/sensor/AccelSeonsor.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:885597c202d2728f8c44805dfca2beb1c89057660ffaa043991f7de3e6d37a7c
+size 2127
