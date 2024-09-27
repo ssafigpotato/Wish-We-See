b6fe4f7a57fcba1e8784f6e1ce3e015c918c5f70
LeeSpect <wewqwew153@gmail.com>
2024-09-27 15:38:54 +0900

    Feat: UserSignUpRequest 구현, User에 누락된 name 필드 반영(S11P21A202-492)

---
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/UserSignUpRequest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa0af002c06b7c7503ba389f7e73500ad2fec56ea0b213be2159b24d9df567e5
+size 1508
--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/entity/User.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e7d34a28637a33579efa365440d97d48c315319b01b98aa71bf783429ec2936
-size 2201
+oid sha256:d90ba37af41d2b93aeede62545d6a884083a5a4da01d49c82df46db3b474ba5d
+size 2266
