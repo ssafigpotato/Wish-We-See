54ce92a9d6b37251981302f18d31008869584bcc
LeeSpect <wewqwew153@gmail.com>
2024-09-30 14:25:46 +0900

    Feat: UserSignUpRequest의 face_img 필드에 setter 추가, faceImg 필드를 MultipartFile 객체로 변경(S11P21A202-674)
    Former-commit-id: a7ef875be642a06d6b2926ec26aa260f00ebcf48

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/UserSignUpRequest.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/UserSignUpRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa0af002c06b7c7503ba389f7e73500ad2fec56ea0b213be2159b24d9df567e5
-size 1508
+oid sha256:f98f51adfa55699873d6eb8072fad171c223c153c3f89a54c9dbccc6f18f07ad
+size 1619
