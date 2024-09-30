b648cd0e39f1790dc3d7df04fb0a90dcf15bf27b
LeeSpect <wewqwew153@gmail.com>
2024-09-30 10:57:43 +0900

    Feat: User Service에 회원가입 메서드 구현(S11P21A202-674)
    Former-commit-id: 8c8dd898cbc7aa638692594dbf61ada15c9c9037

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserService.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserService.java
@@ -1,8 +1,12 @@
 package com.ssafy.missing_back.domain.users.service;
 
+import com.ssafy.missing_back.domain.users.model.dto.request.UserSignUpRequest;
+import com.ssafy.missing_back.domain.users.model.dto.response.UserFindResponse;
 import com.ssafy.missing_back.global.auth.jwt.dto.JwtToken;
 
 public interface UserService {
+
+	UserFindResponse signUp(UserSignUpRequest userSignUpRequest);
 
 	JwtToken signIn(String loginId, String password);
 
