2e8c35b4bb78d9a4622cf24466906176835ea4f2
inception010203 <inception010203@gmail.com>
2024-09-30 14:38:30 +0900

    Feat: 백그라운드 가속도센서 테스트를 위한 console 코드 추가(S11P21A202-594)
    Former-commit-id: 60f4a3daf616cedb70048777ec14a4a859399dde

--- client/components/sensor/BackgroundAccelTask.tsx
+++ client/components/sensor/BackgroundAccelTask.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3d22c6cff8ca8e85ebcdc253776fe74e7040d4f65e0453b14115d7b6e7b8d64
-size 2556
+oid sha256:5c51b9cd850d3ca76e88ef30fa6cb3c90f1deb7cbaee986ee2c0ab325d5e637f
+size 2729
