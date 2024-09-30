8d56ee7bb168bc003f0a5d464ff760dcc2d710cb
jwun95 <jwun95@naver.com>
2024-09-30 15:31:11 +0900

    Feat: Location Background 기본 코드 구현 & 기본 React Native 코드 삭제
    Former-commit-id: 6cbd0fe1d9dadbcd7daec432b43457539ddc85be

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fcb7f74628f1ca2d7cbd751c8da5a9cfdf64e1075b5221f42f9bb4a7a2ac8d5
-size 991
+oid sha256:ff904eb7290695bab20bf798327466959f88bca11d203f4138f4b118be52c03e
+size 248
