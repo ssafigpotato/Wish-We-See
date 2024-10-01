407b86e9d854e414ab2b7b6c935d50877ed47027
LeeSpect <wewqwew153@gmail.com>
2024-10-01 13:13:57 +0900

    Feat: User Service에 연락망 추가 메서드 생성(S11P21A202-674)
    Former-commit-id: eac3ba94b1ba8cebd4b2a1c21774a31e9427f35a

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05acd46ea5c94ff5508c96f23385f0af35d22bca2dfcf8badf2e748f7cb26460
-size 3795
+oid sha256:41afc79201d0c80c231a93d1363f4401609b63768284b79f055a5f321374cd21
+size 4005
