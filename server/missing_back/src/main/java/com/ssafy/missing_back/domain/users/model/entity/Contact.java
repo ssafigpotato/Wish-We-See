0e311ba885914807b7dad8a99c4df2246fc0c80f
LeeSpect <wewqwew153@gmail.com>
2024-10-01 14:15:27 +0900

    Fix: Contact 엔티티에 잘못 작성된 JoinColumn의 name 부분 수정(S11P21A202-674)
    Former-commit-id: 0ab2df9459d26ab0c26d8e8ae7bb6ce4853653ff

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/entity/Contact.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/entity/Contact.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2b979c9d7cb7d8458e474ead00ffb0ecaad06aacd1fd34be131b49e97d00266
-size 1162
+oid sha256:ae27e946e3db57cb60895c4e67b6b268fc610b0b5bb93b289122b5452bd4a20a
+size 1161
