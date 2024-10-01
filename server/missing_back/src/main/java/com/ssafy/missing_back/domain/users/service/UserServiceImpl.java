27de2c4d5448029a4138be8874fa9523f1c89d45
LeeSpect <wewqwew153@gmail.com>
2024-10-01 13:23:38 +0900

    Feat: User 엔티티에 OneToMany로 contacts를 담는 필드 추가(S11P21A202-674)
    Former-commit-id: 6f3a125ef531394c368a279a2f2b88be70b6d9a6

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65946ad750b20a08f52a5ef3e46092d6db88cbd6bb76d274daff70397ebb3bed
-size 4003
+oid sha256:3159e5922e658ba1a6715e4ec496e5c6922ac85e6d5e016c4b07d59b463d00ed
+size 4239
