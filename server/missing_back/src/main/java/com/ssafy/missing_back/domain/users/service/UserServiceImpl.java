2b33e7c594580f5683e31d2a46a575e561e55f38
LeeSpect <wewqwew153@gmail.com>
2024-10-01 14:10:10 +0900

    Feat: User Service의 createUserContacts메서드 구현 완료(S11P21A202-674)
    Former-commit-id: 2b6eb8e15f4cfc09095d9e80a9bf675b65534c3c

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba0b4c79d5a8a6a5378b0ec074100c059bdec9a06a9d2bf669bfe2d1b1e8ced6
-size 4154
+oid sha256:f5108159bd76976c9cbd6864124fcab3660c6413947dab82f0e2f33dbad9b959
+size 4411
