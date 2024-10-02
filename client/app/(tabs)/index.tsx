20bb505d2e4ad57a7e93856ba50465ee326f9ed8
jwun95 <jwun95@naver.com>
2024-10-02 11:24:21 +0900

    Feat: Voice Component 구현
    Former-commit-id: d6504c9f7d0f929aa21f53b42e821fda77627683

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:050ed1093de39b745b10f2b7410bcdf0ef437e1f51ca929f1b9a104374fc436c
-size 157
+oid sha256:d47c5a4aacbee7d67b5094d78ee5ff0175efe87a8f27a2b2ae83d451514b2381
+size 317
---
+++ client/components/Voice.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e52eecc0cc4b2f6b8eded595aa2afebc3344503316c6862cdf0cb33495c3820
+size 2484
