6e6a5286234eef0b8b5bcc678be574df3ecbd02b
jwun95 <jwun95@naver.com>
2024-10-02 13:08:54 +0900

    Feat: 음성 포그라운드 녹음 기능 구현
    Former-commit-id: 082bf8dfecf4a232bf3254a1b1ff31b398bedf49

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d47c5a4aacbee7d67b5094d78ee5ff0175efe87a8f27a2b2ae83d451514b2381
-size 317
+oid sha256:135432d82b1a206140e80576e78addae2de62a34bd4e06d3614200edb345cfc9
+size 406
