38d0a7e1b4191aa94d3490f9cda94ee43f331ffe
inception010203 <inception010203@gmail.com>
2024-10-02 15:40:45 +0900

    Feat: 백그라운드 가속도계 테스트 코드 수정본, 빌드파일, 푸쉬알람 추가(S11P21A202-594)
    Former-commit-id: ab7d6f9e3bd1e13b3250187f62f5dcd747ef5146

--- client/components/sensor/BackgroundAccelTask.tsx
+++ client/components/sensor/BackgroundAccelTask.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c51b9cd850d3ca76e88ef30fa6cb3c90f1deb7cbaee986ee2c0ab325d5e637f
-size 2729
+oid sha256:f76950107bf77f638e8bcab4446e5f3b7a0bb5d479d5daaa94b5a2b0751b78f0
+size 12751
