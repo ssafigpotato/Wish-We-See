c4eb3e14205f4adc86368ba719cdabdbfaaadf9c
jwun95 <jwun95@naver.com>
2024-10-02 11:24:21 +0900

    Feat: Voice Component 구현
    Former-commit-id: 6deb79c2c2916d68a2636d18858e1383553881f9

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
+import VoiceComponent from "@/components/Voice";
+import LocationComponent from "@/components/Location";
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>홈입니다.</Text>
+      <Text>홈입니다.???</Text>
+      <VoiceComponent />
+      <LocationComponent />
     </View>
   );
 }
