6dc4a60f6b95e6b86fd036b24d580df740138789
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-02 18:03:41 +0900

    Feat: Input 원자 개발 기본 세팅
    Former-commit-id: 2173babbfb09f9ce22face14d5343808065db15e

---
+++ client/components/atoms/Input/Input.styles.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ce2b494251da2e6b3a45bf13673bc006a687511bf23db103380264f80d11a33f
+size 69
---
+++ client/components/atoms/Input/Input.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1023f28aa220263b4a55ac40884244e2b6e09f6eae67119e1cfcfc1e2aa4200c
+size 118
