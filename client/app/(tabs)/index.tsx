d31a2185fee35c8b97ab9c326df9a667d17acfb8
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-02 16:42:15 +0900

    Refactor: Gyro 센서 코드 분리
    Former-commit-id: 51cea1d1101b2a69d711651a30a7c3329efe6ec4

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd477a40750a406e60588112f06d58a49a921475ab9ba4b6de28f0b6cd9f6839
-size 7192
+oid sha256:b9b6c915400abdd72151b795c4f5b11b858b52baff6ced92009d6a5dab70a915
+size 1841
--- client/utils/GyroSeosor.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6168bad0bb8167f467286a15cee4d7d37b0b16b027d5a7646d47042296684042
-size 2040
