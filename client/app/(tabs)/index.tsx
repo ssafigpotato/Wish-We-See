e7f2f3a377e26074e6564177438ec9a3f359b327
inception010203 <inception010203@gmail.com>
2024-10-04 20:13:08 +0900

    Feat: 버튼 아톰 컴포넌트 생성 및 index 테스트 코드 작성(S11P21A202-408)
    Former-commit-id: 46217352501cf89650e7d528f1eae511725fe2b0

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8656d2721d8a2b60579423a062fcbb48e32074426c749c2f22db239a32e5005
-size 2290
+oid sha256:f19fb179fd3dd168fd6a371fe5d97bbe7f56686bcb3f6abc05b75db9e99453a3
+size 2234
--- client/components/atoms/Button.tsx
+++ client/components/atoms/Button.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1462cb2c3fc4c49d7b4d758b390f676b5aa4cbfee5fd1795a6e19b9cfbf4957b
-size 886
+oid sha256:5e7a830a00b45af5f5fa9bc293ccdb055c608f5912ae5fec3749c7eac3f8453f
+size 1470
