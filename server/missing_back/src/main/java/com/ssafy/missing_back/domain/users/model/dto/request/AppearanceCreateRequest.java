42e31fdb228c20157ae812fc372a9b7b92448330
LeeSpect <wewqwew153@gmail.com>
2024-10-05 19:52:40 +0900

    Feat: Appearance Detail Repository 구현(S11P21A202-640)
    Former-commit-id: 82d74e48cce044805b7a2a3e3882fb8c1f49dab2

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/AppearanceCreateRequest.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/AppearanceCreateRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd076ae86bf6b226e3678da6cc615e2b814c71b9eec2235615f7379e18a03eb5
-size 416
+oid sha256:a7a3840cb0eb964bb0cf8557cb27691b0eb57d3d6ca937cc0d0bb55738af5a96
+size 415
