af6d9780ae60ccabe249b048cf485eabe2fcb72a
LeeSpect <wewqwew153@gmail.com>
2024-10-05 19:36:34 +0900

    Feat: AppearanceCreateRequest 생성(S11P21A202-640)
    Former-commit-id: cf069145e73235d3b8c55cdb5311891bbc2a6b61

---
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/AppearanceCreateRequest.java
@@ -0,0 +1,24 @@
+package com.ssafy.missing_back.domain.users.model.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class AppearanceCreateRequest {
+
+	@Setter
+	@JsonProperty("img_url")
+	private String imgUrl;
+
+	@JsonProperty("top_color")
+	private String topColor;
+
+	@JsonProperty("bottom_color")
+	private String bottomColor;
+
+	private String detail;
+
+
+}
