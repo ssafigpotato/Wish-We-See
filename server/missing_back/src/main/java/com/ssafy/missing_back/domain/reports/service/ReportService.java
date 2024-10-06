3f5dae9a8de616b2ec26849ca3b2cab8e0768bd3
LeeSpect <wewqwew153@gmail.com>
2024-10-06 17:57:39 +0900

    Refactor: 위험상황 추가하려고 하는 중(S11P21A202-649)
    Former-commit-id: fa16b0d9ff6f4661f5570ce08aaefd20df3f207f

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/service/ReportService.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/service/ReportService.java
@@ -1,12 +1,12 @@
 package com.ssafy.missing_back.domain.reports.service;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.ssafy.missing_back.domain.reports.model.dto.request.ReportCreateRequest;
 import com.ssafy.missing_back.domain.reports.model.dto.response.ReportDetailResponse;
 
 public interface ReportService {
 
-	ReportDetailResponse writeReport(ReportCreateRequest reportCreateRequest) throws IOException;
-
+	ReportDetailResponse writeReport(Long dangerousSituationId, ReportCreateRequest reportCreateRequest) throws IOException;
 }
