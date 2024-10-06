d20ede9af8042ab44faa2ff67a64fde87b51712a
LeeSpect <wewqwew153@gmail.com>
2024-10-06 19:00:36 +0900

    Refactor: Report Image 관련 파일 삭제, 그 외 일부 관련 코드 삭제(S11P21A202-649)
    Former-commit-id: 2357f69a2e640fd73b140b7766e91814c9f479bc

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/request/ReportImageCreateRequest.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:611c875b372d66daec3f7fe3276175929ca2c52edb34075312618e5cff5e553f
-size 1240
--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/repository/ReportImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.missing_back.domain.reports.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.missing_back.domain.reports.model.entity.ReportImage;
-
-public interface ReportImageRepository extends JpaRepository<ReportImage, Long> {
-}
