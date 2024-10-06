3e0432ddc8518d338c5604b0beb00fbe17d599b9
LeeSpect <wewqwew153@gmail.com>
2024-10-06 21:17:01 +0900

    Feat: ReportService에 제보 상세 조회 메서드 구현(S11P21A202-648)
    Former-commit-id: a14d6e5e8e009bca0f0885b9130d0892a35f10db

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/service/ReportService.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/service/ReportService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78c61f2c63e9b56364d8ebcd08572456f23bf9e80417ae4d05a7d2e8188908a1
-size 481
+oid sha256:c6a951002d0da0b5866d68aa14249224a43f3336ed824e1a8119a94110f96fb2
+size 615
