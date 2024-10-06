dc14449befb52938b65ec487d442ab39400a4dab
LeeSpect <wewqwew153@gmail.com>
2024-10-06 17:27:49 +0900

    Feat: ReportImage 엔티티에 imgOriginName과 imgStoredName 피드 추가(S11P21A202-649)
    Former-commit-id: b1aa9c7172d8c9245ac1c07e02f3fbde9c99e264

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/entity/ReportImage.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/entity/ReportImage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bebfefb6a02d823e677ce1b871c6a0ef7797155db2c51a446f1e81ac8fed385
-size 1039
+oid sha256:3af43e862b661e1ff9880c92f1247c7688979ff4f0f63f55d48362c2e2320ef1
+size 1173
