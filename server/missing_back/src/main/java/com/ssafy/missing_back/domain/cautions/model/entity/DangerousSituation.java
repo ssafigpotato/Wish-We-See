67b40d775d2692916198c8188ba147622c4dcacb
LeeSpect <wewqwew153@gmail.com>
2024-10-06 19:34:12 +0900

    Refactor: DangerousSituation에서 잘못 mappedBy되어 있던 부분 수정(S11P21A202-649)
    Former-commit-id: 80dd380b5ab703f62c88cee8ebfdfeabffcadb07

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/entity/DangerousSituation.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/entity/DangerousSituation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a1e801a2ff38c0f196ae93e9c57f32ab5065c8f10e7900a3a00dd02fdfa7975
-size 1975
+oid sha256:1a5309da420e4b52624bec75642347eb095e55f5def71fc1fd1cbd16c6fe8211
+size 1987
