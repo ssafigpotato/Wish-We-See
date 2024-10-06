86fce5eb8bb877cb3bff3e5aca65dd25c5cde600
LeeSpect <wewqwew153@gmail.com>
2024-10-06 21:15:31 +0900

    Feat: ReportDetailResponse Dto 구현(S11P21A202-648)
    Former-commit-id: c68c4a55120abf629a43e3397c5790b7453817ef

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/response/ReportResponse.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/response/ReportResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a47ac2fb7cbad5af9f45211836ab1accb592e523812b25c8431e9546ee84bee3
-size 1025
+oid sha256:ef061dce7385523398e3304fc80a04e69582d8059f6bcd2dbd48db8cb7f16a2d
+size 1001
