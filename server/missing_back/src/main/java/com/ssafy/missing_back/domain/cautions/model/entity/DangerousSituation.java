2b1a497c3dd93068da3da09feecb10dae56df576
LeeSpect <wewqwew153@gmail.com>
2024-10-06 17:22:27 +0900

    Feat: DangerousSituation 엔티티에 OneToMany로 reports 필드 구현(S11P21A202-649)
    Former-commit-id: 7ece1a4d816ef73dba2a07d68af3aebda9c97e96

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/entity/DangerousSituation.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/entity/DangerousSituation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96fa51d6f595eccd8bea1dd7a965dc1879cec911edd0f1332e0e5f3521fb710c
-size 1737
+oid sha256:4a1e801a2ff38c0f196ae93e9c57f32ab5065c8f10e7900a3a00dd02fdfa7975
+size 1975
