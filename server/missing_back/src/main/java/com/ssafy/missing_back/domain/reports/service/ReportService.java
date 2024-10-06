1a1daf27810122607159d13c9bd85947da88edb5
LeeSpect <wewqwew153@gmail.com>
2024-10-06 17:52:10 +0900

    Feat: ReportService에 writeReport 메서드 구현(S11P21A202-649)
    Former-commit-id: f0457cdbeb4591e8c150b045f0513ff8c7cd0bc2

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/service/ReportService.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/service/ReportService.java
@@ -1,5 +1,12 @@
 package com.ssafy.missing_back.domain.reports.service;
 
+import java.io.IOException;
+
+import com.ssafy.missing_back.domain.reports.model.dto.request.ReportCreateRequest;
+import com.ssafy.missing_back.domain.reports.model.dto.response.ReportDetailResponse;
+
 public interface ReportService {
+
+	ReportDetailResponse writeReport(ReportCreateRequest reportCreateRequest) throws IOException;
 
 }
