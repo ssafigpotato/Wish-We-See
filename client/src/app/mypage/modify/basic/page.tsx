e0f3a281cd12aec6d4698caa8ae2ec4eb685991f
inception010203 <inception010203@gmail.com>
2024-10-07 16:46:12 +0900

    Feat: 기본  정보 수정을 위한 폼 생성(S11P21A202-845)
    Former-commit-id: 5ac9e7823f5716c976f5ac39c21c4b2387c3eec8

--- client/src/app/mypage/modify/basic/page.tsx
+++ client/src/app/mypage/modify/basic/page.tsx
@@ -1,19 +1,3 @@
-import styles from "./basic.module.scss";
-import { FaArrowLeft } from "react-icons/fa";
-
-export default function BasicModify() {
-  return (
-    <div className={styles.container}>
-      <button>
-        <FaArrowLeft />
-        뒤로가기
-      </button>
-      <div className={styles.title}>기본 정보</div>
-      <div>사진</div>
-      <div>사진 입력 폼</div>
-      <div>성별 폼</div>
-      <div>키 폼</div>
-      <div>전화번호</div>
-    </div>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c2cf3ec5b80827a8c2ed8d9dc21bf13a027f779ddffc3eb01768ac796b693810
+size 2825
