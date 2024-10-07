a5f3280a4e9a0ea432f95fcdacc6fdf75fee069f
inception010203 <inception010203@gmail.com>
2024-10-07 15:54:07 +0900

    Feat: 마이페이지 기본 정보 수정 페 골격 생성(S11P21A202-845)
    Former-commit-id: 6bcd5a6ef29cec519934aa5e9d9a6c6ea322c850

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,16 +1,3 @@
-import nextPwa from "next-pwa";
-import path from "path";
-
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(import.meta.url, "styles")],
-  },
-  productionBrowserSourceMaps: false,
-};
-
-export default nextPwa({
-  dest: "public",
-})(nextConfig);
+version https://git-lfs.github.com/spec/v1
+oid sha256:4bd9b2fe0d381fc800dc1b102e9f06bff736fd760df180722995aa50f8bd22ec
+size 327
---
+++ client/src/app/mypage/modify/basic/page.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:de5f268f3e08ff7024a54a62ddd9c578f235bc2569b86277d55c7ad6b69d6fd1
+size 353
