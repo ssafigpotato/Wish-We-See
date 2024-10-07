e519206efef5f3b80748bba6ee8a1a02e808773e
inception010203 <inception010203@gmail.com>
2024-10-07 19:16:34 +0900

    Fix: plus 버튼 클릭시 보관함에 진입하도록 수정(S11P21A202-845)
    Former-commit-id: ce08ec0f6d1929585614c52bf6c46ffcc5a4469a

--- client/src/app/mypage/modify/basic/page.tsx
+++ client/src/app/mypage/modify/basic/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6d7ed54d311a74a4d0f51c4c788addd0eb2edc1c0311ab197c75e187740858b
-size 4647
+oid sha256:44c500f393a7a551cd547d1c3bb9edb92f7bca07be5e88d8b1bb6b263e671256
+size 4698
