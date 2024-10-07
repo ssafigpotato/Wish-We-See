97f8965ec0ec41a7edc9ce193d63d41ff59c5bb2
inception010203 <inception010203@gmail.com>
2024-10-07 13:57:05 +0900

    Feat: 인상 착의 카드 생성(S11P21A202-567)
    Former-commit-id: 3a11cd5d01684b422c44cb75316c3f70e62ac7d9

---
+++ client/src/app/mypage/_component/_clothes/clothes.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5be597b3976e247c5fccc4ac7ad609ade2c297b2b259615d61b3f4d172bbf020
+size 468
--- client/src/app/mypage/page.tsx
+++ client/src/app/mypage/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81478edcbb9285395c2b59018e381b1ec70e8ccec11152da6798274276af9e74
-size 473
+oid sha256:552ec2ef07b6d093bdcee53375955fa73fc3d4b0dbc254af4bd7a30bc825c825
+size 504
