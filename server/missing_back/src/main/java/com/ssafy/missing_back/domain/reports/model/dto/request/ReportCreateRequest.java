d352c38f795844830a622e73a6455c8d91951652
LeeSpect <wewqwew153@gmail.com>
2024-10-07 13:32:21 +0900

    Refactor: ReportCreateRequest Dto에서 location 받아오는 필드를 float 값으로 받아오도록 수정(S11P21A202-645)
    Former-commit-id: 65811b6459fcec20e3a8919fe8802e279b6be53b

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/request/ReportCreateRequest.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/request/ReportCreateRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a62b720da99a078eda0b9ca0230c030ee3a494f11e6bd96950ba0ddc7083ca46
-size 900
+oid sha256:5994b170f145ed04b8a13bceafeb884a94829f372ad561268f2df350834be510
+size 1063
