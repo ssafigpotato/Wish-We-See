13776e7935bcf0879f2f6410faacb5b2ed9c4645
dongyeon <uuas5866@naver.com>
2024-10-07 15:59:47 +0900

    Fix: voiceserviceimpl 수정
    Former-commit-id: 23569b3be255229ba5e894c42d62f035ef2ef383

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/VoiceServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/VoiceServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a620eefc6a8df96ab306fb36b230fd9a4446bba2f3f878e353b6578fdc50dea
-size 3361
+oid sha256:f656855ae22eeaa811a4eea0b1fff62892be7664cf100e63c120704d326fa870
+size 3362
