b065d954f08079f820dc81dc61aa4d306c32e579
inception010203 <inception010203@gmail.com>
2024-10-07 13:57:05 +0900

    Feat: 인상 착의 카드 생성(S11P21A202-567)
    Former-commit-id: e9a44955bf70f14bdb8ca2c626e8445a4c818a68

--- client/src/app/mypage/page.tsx
+++ client/src/app/mypage/page.tsx
@@ -1,14 +1,14 @@
 import styles from "./mypage.module.scss";
 import BasicInfo from "./_component/_basicinfo/basicinfo";
+import Clothes from "./_component/_clothes/clothes";
 
 export default function Mypage() {
   return (
     <div className={styles.container}>
       <div className={styles.title}>기본 정보</div>
       <BasicInfo />
-
       <div className={styles.title}>인상 착의</div>
-      <div> 인상착의 카드 </div>
+      <Clothes />
       <div className={styles.title}>지인 정보</div>
       <div>이름 전화번호~~</div>
     </div>
