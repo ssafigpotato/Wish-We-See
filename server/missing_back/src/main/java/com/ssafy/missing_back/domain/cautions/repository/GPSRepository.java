f4229ec0fd2dcb5648da0711a49ab74ab694468e
LeeSpect <wewqwew153@gmail.com>
2024-10-07 14:05:38 +0900

    Feat: GPSRepository 생성(S11P21A202-645)
    Former-commit-id: 8d0e1674095b1fe22b2ae8f58921116f34e2157b

---
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/repository/GPSRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.missing_back.domain.cautions.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.missing_back.domain.cautions.model.entity.GPS;
+
+public interface GPSRepository extends JpaRepository<GPS, Long> {
+}
