e6a2934f9567bcd6cc6606aebf3111793f9f49b1
inception010203 <inception010203@gmail.com>
2024-10-07 20:09:53 +0900

    Feat: 마이페이지 인상착의 등록 페이지 생성(S11P21A202-864)
    Former-commit-id: d7e30b46c8ff5151cc02da23d81207041cc3e48d

--- client/src/hooks/useUserContext.ts
+++ client/src/hooks/useUserContext.ts
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0a34f418565f89e3744cd6cb9783006926c294bc7214273442a8b52dba0c1a56
-size 293
+import { useContext } from "react";
+import { UserContext } from "@/context/useUserContext";
+
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+
+  if (!context) {
+    throw new Error("useUserContext must be used within a UserProvider");
+  }
+
+  return context;
+};
