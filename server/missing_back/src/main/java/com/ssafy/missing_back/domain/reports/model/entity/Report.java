d8a8bd2d1d3d439c2d3c3769f3c635762ff65661
LeeSpect <wewqwew153@gmail.com>
2024-10-07 13:28:01 +0900

    Refactor: Report 엔티티의 reportLocation 엔티티의 자료형을 Point로 수정(S11P21A202-645)
    Former-commit-id: 776019c1bd76764a4ce828ff97a689fb91801058

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/entity/Report.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/entity/Report.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f7308c84b89dec96f81336dc4b6a91f278ec29e475879e35699dac25c18c989
-size 1533
+oid sha256:ed2e6e10a66515d53c556aeb187a692c11f80bd7b6b31e52fd4ed63c568b38cc
+size 1952
