ce2721c47e76528ecbf8065957ae632b1bd31d34
inception010203 <inception010203@gmail.com>
2024-10-07 14:33:41 +0900

    Feat: 지인 정보 리스트 생성(S11P21A202-567)
    Former-commit-id: 32a7cb44a859f76d12f51770b679d259967d5758

---
+++ client/src/app/mypage/_component/_friendslist/friendslist.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3d8a52c68ee3629418494ffc25dc6c5765eca16c91a2cc29c7dbf87123cd67e
+size 874
--- client/src/app/mypage/page.tsx
+++ client/src/app/mypage/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:552ec2ef07b6d093bdcee53375955fa73fc3d4b0dbc254af4bd7a30bc825c825
-size 504
+oid sha256:0175cef81ca57cf9a8b47bfe7a0e9aa0718201a055914abac1e6bb4598c848ee
+size 552
