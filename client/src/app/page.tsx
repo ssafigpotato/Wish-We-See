c88d31a63b031827d98f6cd6bcca9d7b17c735af
jwun95 <jwun95@naver.com>
2024-10-07 19:27:09 +0900

    Feat: Camera 기능 구현
    Former-commit-id: 34397ca671d967c38dc9986e72a2da5286b86a18

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,21 +1,3 @@
-"use client";
-
-import styles from "./page.module.scss";
-import Main from "./home/page";
-import { usePermissions } from "@/hooks/usePermissions";
-import Header from "@/components/Header/Header"
-import HeaderArea from "@/components/Header/HeaderArea";
-export default function App() {
-
-  usePermissions();
-
-  return (
-    <>
-      <HeaderArea />
-      <div className={styles.test}>
-        <Header />
-        <Main></Main>
-      </div>
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ca3344661d906b4e9425718bceb09a68fb65c2f7c69ade6bf95e253f6f1701d
+size 569
