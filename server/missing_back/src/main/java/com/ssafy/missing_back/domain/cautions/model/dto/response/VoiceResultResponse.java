f94d92deb46fa5bbdda793ab46ba052b491b01c1
dongyeon <uuas5866@naver.com>
2024-10-07 16:45:07 +0900

    Fix: RestTemplateConfig 수정
    Former-commit-id: adef4b7807dc0bc938397054fd5202d6bf703041

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/dto/response/VoiceResultResponse.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/dto/response/VoiceResultResponse.java
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4a97bd0ccca34ab43249342e7471ab804c0908a308a00a57fe2153a3df263161
-size 446
+package com.ssafy.missing_back.domain.cautions.model.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class VoiceResultResponse {
+
+	@JsonProperty("classification")
+	private String classification;
+
+	public static VoiceResultResponse toDto(String classification) {
+		return VoiceResultResponse.builder()
+			.classification(classification)
+			.build();
+	}
+}
--- server/missing_back/src/main/java/com/ssafy/missing_back/global/config/RestTemplateConfig.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/global/config/RestTemplateConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acfd15a464691a5fe928d696ea38a882079008e4afed22afe4435993e5384412
-size 343
+oid sha256:d03a8a14c830eb684199db26bd658d6b8341c7eb7ae2a8635c32c53e452516f8
+size 617
