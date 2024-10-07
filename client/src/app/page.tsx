71d9b9e0e22dc51dee475af99e03e27383164e45
jwun95 <jwun95@naver.com>
2024-10-07 09:53:25 +0900

    feat: Sensor 권한 체크
    Former-commit-id: 680565d76fd626f79598056085fb66b27b7f2467

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import styles from "./page.module.scss";
 import Main from "./home/page";
+import { usePermissions } from "@/hooks/usePermissions";
 
 export default function Home() {
+
+  usePermissions();
+
   return (
     <div className={styles.test}>
       <Main></Main>
---
+++ client/src/hooks/usePermissions.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4a20b20d58ac1f8f4c78374b1c1fb6270249fd73e20e08384774b00a12789f8f
+size 2529
