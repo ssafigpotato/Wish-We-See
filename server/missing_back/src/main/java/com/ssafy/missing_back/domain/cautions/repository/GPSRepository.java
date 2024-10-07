47de7766c8e661be66adeeab975de1d8c5d8f0fd
LeeSpect <wewqwew153@gmail.com>
2024-10-07 17:11:20 +0900

    Refactor: GPSRepository 쿼리 메서드 수정(S11P21A202-645)
    Former-commit-id: ac1c39143889d92a907ad0be1c93b4af56332ab1

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/repository/GPSRepository.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/repository/GPSRepository.java
@@ -6,6 +6,6 @@ import com.ssafy.missing_back.domain.cautions.model.entity.GPS;
 
 public interface GPSRepository extends JpaRepository<GPS, Long> {
 
-	// situationId를 주면, 가장 최신의 GPS 정보를 가져온다.
-	GPS findTopBySituationIdOrderByCreatedAtDesc(Long situationId);
+	GPS findTopByDangerousSituation_SituationIdOrderByCreatedAtDesc(Long situationId);
+
 }
