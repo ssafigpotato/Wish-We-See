3eeb1eb68ddc85d825784130b61287a187e4d326
LeeSpect <wewqwew153@gmail.com>
2024-10-07 19:57:57 +0900

    Feat: AI쪽에 사진 보내주는 기능 추가(S11P21A202-640)
    Former-commit-id: 694148346344e3cc812e252baac18e19b3a9520e

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/AppearanceDetailServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/AppearanceDetailServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c753aac80918ad197f4370707ffb7276bba6f978efd976bc4a84a3ce9afead6
-size 2311
+oid sha256:6241f9b7f5dc998950e94217c276aa74dc3465e0e3d87ccf47bee63a8917441b
+size 3418
