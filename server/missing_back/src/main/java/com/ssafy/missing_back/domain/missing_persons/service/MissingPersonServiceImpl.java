70180094975b6445f6ec462e2d3327a280c481bc
LeeSpect <wewqwew153@gmail.com>
2024-10-07 16:59:29 +0900

    Feat: MissingPersonService에서 getMissingPersonDetail 메서드 구현 완료(S11P21A202-645)
    Former-commit-id: 70111015b3e58fad9ef2c709a73e52ee3d986c35

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88e1d6afbf375629b0f7fe278fbff300172a80463da51bbfc945a52c7a311af2
-size 1703
+oid sha256:4aeb1f05b529b804601e87a11268d6631db729cbb91b4c6db3a5f29354ceb79f
+size 1858
