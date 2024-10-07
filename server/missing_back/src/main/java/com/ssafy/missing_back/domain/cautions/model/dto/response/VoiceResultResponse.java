2461f83397e771f9b1bb23fa8ed7d20df7874621
dongyeon <uuas5866@naver.com>
2024-10-07 17:37:03 +0900

    Fix: VoiceResultResponse 수정
    Former-commit-id: a18d157584386bc50be4f3ae04dca6d16a9e9794

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/dto/response/VoiceResultResponse.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/dto/response/VoiceResultResponse.java
@@ -2,11 +2,13 @@ package com.ssafy.missing_back.domain.cautions.model.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class VoiceResultResponse {
 
 	@JsonProperty("classification")
