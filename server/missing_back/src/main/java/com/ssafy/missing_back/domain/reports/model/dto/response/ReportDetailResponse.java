d9fe22a3c6c244172f783a9dd2da3945b292a13d
LeeSpect <wewqwew153@gmail.com>
2024-10-07 16:26:55 +0900

    Refactor: ReportDetailResponse의 위치 필드 자료형 수정(S11P21A202-645)
    Former-commit-id: 7ff77d8c3a46c2b03d539433c640c86e40a2b03a

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/response/ReportDetailResponse.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/response/ReportDetailResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bfd50b70c9a99777e1f6bae09248b65bb198b227f05b4d60459f1c4d13b0bc3
-size 1280
+oid sha256:59d7caf1fb2365eb6bb987fdee6e3331797a1b3e90addc7433f5632b48089a0a
+size 1151
