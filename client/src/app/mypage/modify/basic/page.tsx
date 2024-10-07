8e373e1ccb620b562924205c0828cc42c4463f0a
inception010203 <inception010203@gmail.com>
2024-10-07 15:59:27 +0900

    Feat: 뒤로가기 버튼 생성(S11P21A202-845)
    Former-commit-id: da2222de42d7f5d913a4cdd6901f50c8964ffc0f

--- client/src/app/mypage/modify/basic/page.tsx
+++ client/src/app/mypage/modify/basic/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de5f268f3e08ff7024a54a62ddd9c578f235bc2569b86277d55c7ad6b69d6fd1
-size 353
+oid sha256:5902907a9b6a43095a984affd103d906f304db94c0ed7e94b00a01204187bf42
+size 471
