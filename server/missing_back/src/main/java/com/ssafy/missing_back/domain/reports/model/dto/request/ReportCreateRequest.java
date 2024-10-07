de8b778131aea064e72cc0fc26ee3ddd8247af10
LeeSpect <wewqwew153@gmail.com>
2024-10-07 16:17:36 +0900

    Refactor: ReportCreateRequest의 위치 필드 자료형 수정(S11P21A202-645)
    Former-commit-id: b1aa5a56e8ebb42fc915437424b7c94065afec84

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/request/ReportCreateRequest.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/request/ReportCreateRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5994b170f145ed04b8a13bceafeb884a94829f372ad561268f2df350834be510
-size 1063
+oid sha256:6005e48b9120103bd37b1e055b6b36f8b2960d242b7d0716b6b5051ef07ba6d1
+size 1070
