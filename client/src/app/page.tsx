ae0c5be40d8f9433ec27f420d038a0b9aac21494
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-07 17:29:32 +0900

    Fix: Header 공통화로 인한 코드 수정
    Former-commit-id: d52ffe31ce2e5cce4782df766708c22296ccab46

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2d14559b63896fd8e2be09f81da5c0416a6ae211c1a5d520140fd6664a35033
-size 283
+oid sha256:fa22973e39236fa84e4de157bb0a7c0cc462f62628a25aaf921e0ed1c1c56d98
+size 448
