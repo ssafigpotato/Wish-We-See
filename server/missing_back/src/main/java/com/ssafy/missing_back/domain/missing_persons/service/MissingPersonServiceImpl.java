deeeec80a763a8496b2d6d442590b02dfb1d3dbf
LeeSpect <wewqwew153@gmail.com>
2024-10-07 16:14:24 +0900

    Refactor: Report 객체의 위치값 수정(S11P21A202-645)
    Former-commit-id: 4adfad91ffa55cf2676a4d2ac9b88d8a026d599c

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1e3af950534bcd6ad0f80ef268fc5d0d7b4f99f2ee69f56c2c0e92c367864cb
-size 1823
+oid sha256:88e1d6afbf375629b0f7fe278fbff300172a80463da51bbfc945a52c7a311af2
+size 1703
