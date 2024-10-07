da7763df3f991b163d895d9d3a4c664bc3f25e0c
LeeSpect <wewqwew153@gmail.com>
2024-10-07 17:42:24 +0900

    Feat: MissingPersonService에 getAllMissingPersonDetails 구현(S11P21A202-644)
    Former-commit-id: 65bb56f453600a20747d4ef011db2f5456770881

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonService.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonService.java
@@ -1,8 +1,12 @@
 package com.ssafy.missing_back.domain.missing_persons.service;
 
+import java.util.List;
+
 import com.ssafy.missing_back.domain.missing_persons.model.dto.response.MissingPersonDetailResponse;
 
 public interface MissingPersonService {
+
+	List<MissingPersonDetailResponse> getAllMissingPersonDetails();
 
 	MissingPersonDetailResponse getMissingPersonDetail(Long situationId);
 }
--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e145a66728bb876d7c8ff79da3b5be9cd9ed08e445e508cdfca7a119f743e58
-size 1877
+oid sha256:3f2580e7fe48064db5eb6a5dce5157eaac76448ae3866e96ef525dea514ba040
+size 2729
