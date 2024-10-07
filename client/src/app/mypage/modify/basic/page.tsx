6855dc32f319093cf64808c807c150397f179205
inception010203 <inception010203@gmail.com>
2024-10-07 16:46:12 +0900

    Feat: 기본  정보 수정을 위한 폼 생성(S11P21A202-845)
    Former-commit-id: 0f8b2cbfa42fe3de047e6160a0993ee94c0626e8

--- client/src/app/mypage/modify/basic/page.tsx
+++ client/src/app/mypage/modify/basic/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5902907a9b6a43095a984affd103d906f304db94c0ed7e94b00a01204187bf42
-size 471
+oid sha256:c2cf3ec5b80827a8c2ed8d9dc21bf13a027f779ddffc3eb01768ac796b693810
+size 2825
