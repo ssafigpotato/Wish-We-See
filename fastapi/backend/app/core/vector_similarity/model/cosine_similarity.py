947ad1ea92403fec3da8db2fba6cb20e3f963796
hybrid417 <hybrid417@naver.com>
2024-10-07 18:14:07 +0900

    Feat: 이미지 벡터화 API, 이미지 벡터 비교 API 설계
    Former-commit-id: 437c144e403cac20243de7f15505f8631a7955d7

---
+++ fastapi/backend/app/core/vector_similarity/model/cosine_similarity.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ff0867f18dfb1f2cfc219037b1613c9fb3a1bfa5e90ba51c1bf6d4ce21637bf
+size 175
---
+++ fastapi/backend/app/core/vector_similarity/service.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:60316f65d7fc21810bf7017ebe719bdebe6a4e0d8c6774e744be46096f5baecb
+size 2077
