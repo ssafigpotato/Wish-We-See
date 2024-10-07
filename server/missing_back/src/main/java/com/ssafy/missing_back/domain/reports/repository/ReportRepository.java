3b61ef651cec9581714647d5454d8861f3752ae0
LeeSpect <wewqwew153@gmail.com>
2024-10-07 10:17:19 +0900

    Feat: Report Repository에 위험 상황 ID로 위험 상황 객체 조회하는 메서드 생성(S11P21A202-647)
    Former-commit-id: b4a3ccee50c078744aef800ac203ff6cc2c0648a

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/repository/ReportRepository.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/repository/ReportRepository.java
@@ -1,8 +1,12 @@
 package com.ssafy.missing_back.domain.reports.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.missing_back.domain.reports.model.entity.Report;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+	List<Report> findByDangerousSituationId(Long dangerousSituationId);
 }
