1d83bc72d1b0bab79c958bfd47262edae19ae099
LeeSpect <wewqwew153@gmail.com>
2024-10-07 16:53:04 +0900

    Feat: GPSRepository에 situationId를 주면, 가장 최신의 GPS 정보를 가져오는 메서드 추가(S11P21A202-645)
    Former-commit-id: ac35b9ddb93e168a63ff99e0290b628ba2411821

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/repository/GPSRepository.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/repository/GPSRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ssafy.missing_back.domain.cautions.model.entity.GPS;
 
 public interface GPSRepository extends JpaRepository<GPS, Long> {
+
+	// situationId를 주면, 가장 최신의 GPS 정보를 가져온다.
+	GPS findTopBySituationIdOrderByCreatedAtDesc(Long situationId);
 }
