939a64f06e7cf47519a6b8d50aeaead44b96d485
LeeSpect <wewqwew153@gmail.com>
2024-10-07 13:38:33 +0900

    Refactor: ReportResponse Dto의 위치 정보 필드 자료형을 float로 변경(S11P21A202-645)
    Former-commit-id: a4626d532e2bf605158da70d9568818ea22fd2e8

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/response/ReportResponse.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/dto/response/ReportResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef061dce7385523398e3304fc80a04e69582d8059f6bcd2dbd48db8cb7f16a2d
-size 1001
+oid sha256:32a531cc97fa69d5513a7852ff02a528b067f378c3c5fc94d2f10f08e81ccc11
+size 1322
