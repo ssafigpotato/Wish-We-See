d0ff18a73e79bef36f64e7683c2a5f0dfbf7a13e
LeeSpect <wewqwew153@gmail.com>
2024-10-07 14:26:57 +0900

    Refactor: 엔티티간 관계성 반영(S11P21A202-645)
    Former-commit-id: 2537596c9196aa7e33d03f041d8f399bbcbc3204

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/entity/DangerousSituation.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/entity/DangerousSituation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a5309da420e4b52624bec75642347eb095e55f5def71fc1fd1cbd16c6fe8211
-size 1987
+oid sha256:39c49465f3f7935c98e8d2391cf6a596f7f74801d66d405b65618a0f8e72b329
+size 2187
--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/entity/User.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb40b7877f595c458d6db45c31909f065ab5e734e80664c6e94690d7da3e584d
-size 3142
+oid sha256:67a4ae31ff06ba982e654ee8cbde37c73f6f10930bbbdf1ef43aa528b719ae5f
+size 3397
