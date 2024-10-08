5c0b90b91fbb8c5ce052f432ca70260105f3a7e1
inception010203 <inception010203@gmail.com>
2024-10-08 15:26:18 +0900

    Feat: 지인 삭제 및 등록 컴포넌트 생성(S11P21A202-870)
    Former-commit-id: 1f7ea24cd1909fff4a51f705ef9976186047ef9e

--- client/src/app/mypage/modify/friends/page.tsx
+++ client/src/app/mypage/modify/friends/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87cbc2b4653104216131c1405e1986b2e3ea208f522445a3578b01cbcc990219
-size 579
+oid sha256:404bb57c0680e38d948eec32e304a0c979fa7c689adce7af17f606447fc0efe1
+size 2151
