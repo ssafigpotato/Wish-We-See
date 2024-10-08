cf5ec0bbc5ef469dcbd64bdbe4d7d80020ea00f3
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-08 18:50:54 +0900

    Chore: next 설정 수정(next/image)
    Former-commit-id: 9b0bd660b258a3a456e867504fa346fb1f6ef1db

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fa22973e39236fa84e4de157bb0a7c0cc462f62628a25aaf921e0ed1c1c56d98
-size 448
+"use client";
+
+import styles from "./page.module.scss";
+import Main from "./home/page";
+import { usePermissions } from "@/hooks/usePermissions";
+import Header from "@/components/Header/Header"
+import HeaderArea from "@/components/Header/HeaderArea";
+export default function App() {
+
+  usePermissions();
+
+  return (
+    <>
+      <HeaderArea />
+      <div className={styles.test}>
+        <Header />
+        <Main></Main>
+      </div>
+    </>
+  );
+}
