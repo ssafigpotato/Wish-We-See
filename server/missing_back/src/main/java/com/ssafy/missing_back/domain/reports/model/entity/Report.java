7175152ee464958afcef6d09868bce05df37efb1
LeeSpect <wewqwew153@gmail.com>
2024-10-08 13:28:47 +0900

    Style: Report 엔티티 컨벤션 적용(S11P21A202-649)
    Former-commit-id: 4b0a64d317b104f492ba9ab5e31663b92ec3efbd

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/entity/Report.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/model/entity/Report.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1943c9728f72c73ab9291fc7161d90a2d3dd92100fc95289609d9168c627af88
-size 1854
+oid sha256:5678eed7191aa1a3fea2fe074efe19ed3a2509b30b2bffb8793a66df3ab40137
+size 1635
