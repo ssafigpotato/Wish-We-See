b7a6bc616b18454cbeb51f06d491a04a16d8fdee
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-08 18:52:39 +0900

    Feat: 실종 상세 정보 페이지 추가
    Former-commit-id: 7f768e872b6f60b59ec74190929f9cb9543a299c

---
+++ client/src/app/report/[id]/layout.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f0729507bca530e7d4a448c31299910dbacbd842b30589b62257532e76eb658
+size 408
---
+++ client/src/app/report/[id]/page.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c36d46041f6d0bba7a96e1c4329df7a5d2c0e487432fbe01898ea95294c63586
+size 1094
