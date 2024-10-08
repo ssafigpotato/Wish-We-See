7cb16b1a19850359de863d549dec55adf4e77d56
dongyeon <uuas5866@naver.com>
2024-10-08 16:27:13 +0900

    Fix: 위험상황 레벨 수정
    Former-commit-id: bee8303866e270aca6e44715011efb2e780bc694

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/VoiceServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/VoiceServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b441fa99b7ccb659bba83a3d7276576861392f6ff87b323a97ce222a68a4b032
-size 4562
+oid sha256:1944f1d74df6ca2be78f1bf5e51326d31135f8424a8fdcf355281501d57ccba8
+size 4675
