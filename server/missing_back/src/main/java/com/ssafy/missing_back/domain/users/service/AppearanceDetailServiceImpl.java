1efc89c19ef2cac867ba3af19d0477713019fbfb
LeeSpect <wewqwew153@gmail.com>
2024-10-08 10:34:11 +0900

    Feat: AppearanceDetailRepository에서 deleteByUserId 구현(S11P21A202-640)
    Former-commit-id: 4ea019aa2b1007a59883d325eaa301866e34d9bf

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/AppearanceDetailServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/AppearanceDetailServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6241f9b7f5dc998950e94217c276aa74dc3465e0e3d87ccf47bee63a8917441b
-size 3418
+oid sha256:6f3b21e8b4198c3d60e5f919424875f5c641e265dcae0adf98e403d5fcf12a9a
+size 3575
