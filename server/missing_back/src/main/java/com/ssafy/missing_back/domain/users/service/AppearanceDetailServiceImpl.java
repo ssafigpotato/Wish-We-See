365ae4dfee426bf7d223f57de10f0b5fb9fa121b
LeeSpect <wewqwew153@gmail.com>
2024-10-08 11:04:44 +0900

    Fix: AppearanceDetailService에서 AppearanceDetail 저장하는 메서드에 엔티티 업데이트하는 로직 추가(S11P21A202-640)
    Former-commit-id: b97578b581922187921edba6e439ee4924519e9e

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/AppearanceDetailServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/AppearanceDetailServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dec256a9016bf229d5a2aa6ecd197ab83879bab81aacec56162510ee0443d469
-size 3643
+oid sha256:9c7409f08d8f3734b85f0a0ce73e8921c14382c6945e6201b2d152bffe755001
+size 3678
