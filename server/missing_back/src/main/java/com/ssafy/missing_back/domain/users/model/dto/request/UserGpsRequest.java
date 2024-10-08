cefac0e27c6fe891201d4a103561f09c8202ce7f
dongyeon <uuas5866@naver.com>
2024-10-08 18:13:25 +0900

    Fix: GPS Fcmtoken 수정
    Former-commit-id: a095da9a0fdbb343902c3174dd78b6eb3df2e629

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/UserGpsRequest.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/UserGpsRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f329aab5820c497f50bec78b0e718640740698d2c9ce4507e7f70ea057c6b741
-size 573
+oid sha256:0e315c1663be3f29e742179ff196d9910a1e8e5bf37c82c53685118d9db6abc8
+size 462
