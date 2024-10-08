e6d26141c770e31f20a3aa6ce56b0509212ebf7a
LeeSpect <wewqwew153@gmail.com>
2024-10-08 09:50:58 +0900

    Feat: AppearanceDetailRepository에 existsByUserId 메서드 추가(S11P21A202-640)
    Former-commit-id: 765a885a48295f296f0a312e79339e89fc20c8da

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/repository/AppearanceDetailRepository.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/repository/AppearanceDetailRepository.java
@@ -1,8 +1,3 @@
-package com.ssafy.missing_back.domain.users.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.missing_back.domain.users.model.entity.AppearanceDetail;
-
-public interface AppearanceDetailRepository extends JpaRepository<AppearanceDetail, Long> {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:075c1ea849a4c75332be00bb0c24549dd04212531f4a563139ff9f27e8f32ff1
+size 328
