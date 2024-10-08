f2f8343d1549251a4a924a30f4378dcfc02f5734
LeeSpect <wewqwew153@gmail.com>
2024-10-08 15:47:50 +0900

    Feat: ReportService 단에서 이미지 잘라서 저장하는 기능(S11P21A202-649)
    Former-commit-id: a626f6b8e2daef0281f2ee77ac7cff53163839e6

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/UserSignInRequest.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/model/dto/request/UserSignInRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d50550ed6af3fc1ea74f720313c07dd366d36fd3d5f34ac2f0736668f583005
-size 596
+oid sha256:c663c5679cf2ff96a0157deb5611f4625f127b8a22d9dc48c841795c09affcbc
+size 599
