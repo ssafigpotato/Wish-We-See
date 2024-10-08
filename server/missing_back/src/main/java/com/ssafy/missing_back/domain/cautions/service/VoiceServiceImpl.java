e9a38f6ac73292d5215394d2f13bdc8605af1f52
dongyeon <uuas5866@naver.com>
2024-10-08 15:51:05 +0900

    Fix: 위험상황 감지 값 수정
    Former-commit-id: c686a4218d1fcc29f457bc01e9f62496e8db519e

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/VoiceServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/VoiceServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a2c7ceb18e02b580170741967dd7ff385d8eade5ecb524af07915165f190ba5
-size 4559
+oid sha256:b441fa99b7ccb659bba83a3d7276576861392f6ff87b323a97ce222a68a4b032
+size 4562
