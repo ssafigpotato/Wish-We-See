7076ee694ed0081406c690c7fd3adcff7d0147e4
inception010203 <inception010203@gmail.com>
2024-10-08 17:40:53 +0900

    Feat: 지인 사진 및 실종자 사진 추가(S11P21A202-874)
    Former-commit-id: da8e6020ca5353ebb2d50238ad408abfc0861b8f

--- client/src/app/home/_component/_missinglist/missinglist.tsx
+++ client/src/app/home/_component/_missinglist/missinglist.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ba0d0971d3f1a1eb4d1e2e701f0ea68cb9e4fc6561db8784467472ae9f0dd7f
-size 1775
+oid sha256:7077cacfe3895a51fe2b10022eb3a199fe885d825742f1d30b0de25d24142d27
+size 1824
