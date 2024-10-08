76c2c710693fc830c3f55d3a5b75fcdb04fddd7e
dongyeon <uuas5866@naver.com>
2024-10-08 15:57:19 +0900

    Fix: Security url 수정
    Former-commit-id: dca12f9855e481fd06a622ff280ad56a4b5c8410

--- server/missing_back/src/main/java/com/ssafy/missing_back/global/config/SecurityConfig.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e6101542e002a1c4c8d672a374ecac07ed2015b7cb78f2b6d7031b79cb52248
-size 3610
+oid sha256:d68d2229a9092ca98e2c174b399c63cf102b724fc390f9a3c92da8370135565a
+size 3457
