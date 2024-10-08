67c10f55c3e991c227913fb82b2a0b5fe6ca64c3
inception010203 <inception010203@gmail.com>
2024-10-08 16:13:22 +0900

    Feat: 지인정보 새로 등록 및 삭제 기능 구현(S11P21A202-870)
    Former-commit-id: c83ef3e6c2d614f2470c9534e90e9f7472aa1370

--- client/src/app/mypage/modify/friends/page.tsx
+++ client/src/app/mypage/modify/friends/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:404bb57c0680e38d948eec32e304a0c979fa7c689adce7af17f606447fc0efe1
-size 2151
+oid sha256:4eb10474d2c5648cb8937af0f67e75c259fde63d522522bc55bb169e3e60e002
+size 4440
