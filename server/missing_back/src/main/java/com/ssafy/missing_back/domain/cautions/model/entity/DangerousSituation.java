9e58da6571d7ffaff35ef6e29d67813b83036fec
LeeSpect <wewqwew153@gmail.com>
2024-10-08 17:51:46 +0900

    Feat: DangerousSituation 엔티티에 additionalInfos 필드 추가(S11P21A202-643)
    Former-commit-id: 1aad1febb7f038939313c74ec896f3201044a464

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/entity/DangerousSituation.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/model/entity/DangerousSituation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d81131334c13131f75d0ad74d455c55b38fda16e85d2c052dae20f2d40ac7013
-size 2209
+oid sha256:0d8079293e72feac85dc2209ed37df5bbb8440e58160245116ebf27c9c50f94a
+size 2473
