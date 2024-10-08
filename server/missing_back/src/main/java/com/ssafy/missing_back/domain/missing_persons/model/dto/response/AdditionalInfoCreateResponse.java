46c838153c376b409470c044d320a45605aa7234
LeeSpect <wewqwew153@gmail.com>
2024-10-08 19:01:51 +0900

    Feat: 실종자 추가 정보 전송 기능 구현(S11P21A202-643)
    Former-commit-id: 6f97e7abbe871a67f939854da2a538081b393f92

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/model/dto/response/AdditionalInfoCreateResponse.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/model/dto/response/AdditionalInfoCreateResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d744757b0b484adea54d9debc3cd7e83011f077a5c79a77c2252e0ecf3887092
-size 906
+oid sha256:76e63e482f33c66ad019566165f253660d43a7cec70cc2f01e0565fdd13be7eb
+size 900
