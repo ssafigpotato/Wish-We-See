474988d9b379298bfe95f988692404168176fefe
inception010203 <inception010203@gmail.com>
2024-10-08 17:56:58 +0900

    Feat: 실종자 더미데이터 추가(S11P21A202-874)
    Former-commit-id: caa739aebb0c6d7b0272bea012af62d97b6734aa

--- client/src/app/home/_component/_missinglist/missinglist.tsx
+++ client/src/app/home/_component/_missinglist/missinglist.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7077cacfe3895a51fe2b10022eb3a199fe885d825742f1d30b0de25d24142d27
-size 1824
+oid sha256:9246b318228cf8226388c5a54d0a6d440e4bfb92c9c3663154b30a5b2a7006e6
+size 2596
