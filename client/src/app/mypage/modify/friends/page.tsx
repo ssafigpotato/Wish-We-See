03f1b99c6a4582dbf72c10058fc602b9fac905a8
inception010203 <inception010203@gmail.com>
2024-10-08 16:24:23 +0900

    Fix: 지인수정 페이지 CSS 수정 (S11P21A202-870)
    Former-commit-id: e80e532a8cb3c093ce2d92163e0fd38542ecc0a3

--- client/src/app/mypage/modify/friends/page.tsx
+++ client/src/app/mypage/modify/friends/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4eb10474d2c5648cb8937af0f67e75c259fde63d522522bc55bb169e3e60e002
-size 4440
+oid sha256:9a8b5638fca3f8a90554cb450b78c86630916586f11b8f0dc07ab33195a58be5
+size 4516
