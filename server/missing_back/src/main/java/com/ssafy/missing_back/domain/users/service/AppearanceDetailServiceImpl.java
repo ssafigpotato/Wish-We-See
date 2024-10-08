c2553ab47035af15aa945782db6bf145999720d1
dongyeon <uuas5866@naver.com>
2024-10-08 13:03:54 +0900

    Fix: AppearanceDetailServiceImpl 수정
    Former-commit-id: 0b4d88786dd9506010ca8727c7b6eea515bc1df6

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/AppearanceDetailServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/AppearanceDetailServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c7409f08d8f3734b85f0a0ce73e8921c14382c6945e6201b2d152bffe755001
-size 3678
+oid sha256:72824bd1217c5763af3fe792e6bbf9c8fb4b1ff86f242f6c6c7d0b87102f1b4a
+size 3578
