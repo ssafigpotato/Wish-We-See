da630c2f8b58cc525c0b7a6acd9882e6d91de2f3
inception010203 <inception010203@gmail.com>
2024-10-08 10:36:21 +0900

    Feat: 마이페이지 기본정보 수정에 헤더 추가 (S11P21A202-866)
    Former-commit-id: 024e4baf7b3cda32cbdb4f2e4cb852d13b03fa55

--- client/src/app/mypage/modify/basic/page.tsx
+++ client/src/app/mypage/modify/basic/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44c500f393a7a551cd547d1c3bb9edb92f7bca07be5e88d8b1bb6b263e671256
-size 4698
+oid sha256:86f2d5d9ead9aceb67b08034ba05e581d8b6fd798c4631af979b7571babe8c7c
+size 4764
