6b2824ba0d3b4cfe731bef6a518648e509902994
LeeSpect <wewqwew153@gmail.com>
2024-10-08 11:01:31 +0900

    Feat: AppearanceDetailRepository에 findByUserId 메서드 구현(S11P21A202-640)
    Former-commit-id: 29a646b31f3cf3e86e8a0f3d4a76c7631b749208

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/repository/AppearanceDetailRepository.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/repository/AppearanceDetailRepository.java
@@ -1,12 +1,3 @@
-package com.ssafy.missing_back.domain.users.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.missing_back.domain.users.model.entity.AppearanceDetail;
-
-public interface AppearanceDetailRepository extends JpaRepository<AppearanceDetail, Long> {
-
-	boolean existsByUserId(Long userId);
-
-	void deleteByUserId(Long userId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:10711f63ab50aab88fcb0212b98bf1d3f09e9e5c4e41155aa1d33fce39a72bd5
+size 410
