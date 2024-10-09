9095b2b78d1ba61e0335f40e0b23857650ea1584
dongyeon <uuas5866@naver.com>
2024-10-09 18:08:13 +0900

    Fix: Firebase 알림 수정
    Former-commit-id: 3ea355c3953d6c20203748f7142ceac23e75ea4d

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/LatestLocationServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/LatestLocationServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a2fba60db414b90a6a0005b14f8d4311a456f10d468e408e8bcf1b06d62f205
-size 3180
+oid sha256:aa7edc942cc724ad9435d7ae7d1aa5fc727438eaec162d8c92cbb729335d3da3
+size 3091
