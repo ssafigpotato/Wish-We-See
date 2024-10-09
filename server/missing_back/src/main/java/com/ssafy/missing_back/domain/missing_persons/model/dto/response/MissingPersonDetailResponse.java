fe06863bbd771eaed17ac51db4cc8668489b0d9e
LeeSpect <wewqwew153@gmail.com>
2024-10-09 19:04:12 +0900

    .
    Former-commit-id: 4b8e4f0ddf1c85c20224f8814a59f38bdbe818ba

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/model/dto/response/MissingPersonDetailResponse.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/model/dto/response/MissingPersonDetailResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e4b2d83fe5953c1665a8f65163f8ecb52d28e1f2c45e3565aab34a4a5bee7e7
-size 2017
+oid sha256:27d80c6a12cc0a4d8d54b99429f0f8a33ebdd5a7738a69093afc8d99c1154c93
+size 2268
