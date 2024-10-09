2415b2c6f75323a53b3e8a50169ce665b3bef33c
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-09 11:23:14 +0900

    Refactor: Title 컴포넌트 공통으로 분리
    Former-commit-id: e0d82f275ab948e34899eb0a253eed959984f43e

--- client/src/app/friend/_component/Title/Title.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:951521f37862f0dfc70a13d84e0cb4f15ee4e1c3a9d2feed7a3c4bb254a35fa7
-size 437
---
+++ client/src/components/Title/Title.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f289626a39a1643cba09724646cafdb5cd196bd125b27aae921e8a6e1bb6a404
+size 426
