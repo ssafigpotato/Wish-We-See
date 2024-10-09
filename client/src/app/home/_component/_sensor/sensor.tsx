6b8d14fc1749c506a045a984cfb773a6fd3c7ab6
inception010203 <inception010203@gmail.com>
2024-10-09 14:42:03 +0900

    Feat: 센서 정보를 배열에 담아 map으로 돌리도록 html수정(S11P21A202-878)
    Former-commit-id: 74ce878bd7de25323d835b905fc8b0e3310ee7bb

--- client/src/app/home/_component/_sensor/sensor.tsx
+++ client/src/app/home/_component/_sensor/sensor.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8da8f30d0c936166e775ae55d986eda07fdb19064eb909b895e933ac07bf4d1e
-size 2660
+oid sha256:b04ff3bb7b146e86beaa6a29a19f61223cdc04513d702bb579c8ce219845c274
+size 2734
