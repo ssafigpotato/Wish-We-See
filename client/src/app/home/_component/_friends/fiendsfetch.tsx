0ad3cfe3721d35796d16a304dd55f4832263fd92
inception010203 <inception010203@gmail.com>
2024-10-09 17:50:48 +0900

    Feat: 로그인 유저의 지인상태 API 연결(S11P21A202-900)
    Former-commit-id: ad7f0a97d3477caecd1c85a7429fa59131334f7e

---
+++ client/src/app/home/_component/_friends/fiendsfetch.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f2d8ce1cd620d70d9f3bf2752e01e296719b129133cd5b3e101fa0194863632
+size 580
--- client/src/app/home/_component/_friends/friends.tsx
+++ client/src/app/home/_component/_friends/friends.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c95b660b0daf31df980dbd270a2585f9eda4ec9a019af09f52254757aca7bce
-size 549
+oid sha256:b4bf7d792ff7ebd1aba3dc2a2f518ca90dd1cdc13010f24686c4442d3ae61372
+size 2238
