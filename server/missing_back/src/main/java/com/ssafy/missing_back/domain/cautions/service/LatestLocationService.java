4d2ae133b2bd34e7c6baa9f0d9fd99be8be2afe0
dongyeon <uuas5866@naver.com>
2024-10-09 14:08:58 +0900

    Feat: GPS 마지막 위치 값 저장 기능
    Former-commit-id: 7490cee2d5b4a5d252ef8ef4aaea7a96da52c34e

---
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/LatestLocationService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c17ee00d4db1aaf6b2dfebfd7c47e2e108786410c691c7c663bc8ed25267167c
+size 222
---
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/cautions/service/LatestLocationServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c23b39196c7fa68d2b6d4bff3668beeec543c01744e02a333c5aa135a1287aed
+size 2097
