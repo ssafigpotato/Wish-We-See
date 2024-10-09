8c7ce3f8ea8ab464bd55badd260120192d3bfd6d
LeeSpect <wewqwew153@gmail.com>
2024-10-09 13:31:59 +0900

    Feat: ContactRepository에 수신인 ID로 Contacts 리스트를 반환받는 메서드 구현(S11P21A202-644)
    Former-commit-id: 997a91a0334758747b703693381843410d2fce1f

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserService.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b3742fd654954dafd18f9d2b165824ab567b279c92ba1dd9ff9cb534568acaf
-size 764
+oid sha256:6d53b92285e39a6bfe16be78e4dc45be8c44cd8a1569e82a4101d3b021185e95
+size 904
--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ea8c55d7ec50774e82bf7fcccbf3bedad50aa05cd58bee0c17f1dc3afd25ea0
-size 5141
+oid sha256:4256a2e8abe8ba40427716f176020bfbfbab63deebe86a02a8dde96497e8a670
+size 5437
