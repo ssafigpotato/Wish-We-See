6ffba802392b93cb4753d98b82aefd1067ca2710
inception010203 <inception010203@gmail.com>
2024-10-09 19:35:52 +0900

    Feat: missing_person_id에 따라 상세보기로 연결(S11P21A202-903)
    Former-commit-id: efc70f2a4c588d102a6ae727956fd0556867e750

--- client/src/app/home/_component/_missinglist/missinglist.tsx
+++ client/src/app/home/_component/_missinglist/missinglist.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b72da9ad23e19b44b1d0906a06579d324703fe8fc89d4ff030f4211b845a0ea
-size 3337
+oid sha256:654746d834cc33e69b72b77379196eeff592dbe6dbcdb992db4b2bdf1ff244ae
+size 3350
