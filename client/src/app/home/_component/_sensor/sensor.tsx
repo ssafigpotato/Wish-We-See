ba38305862b285869822b71409a8f6e8c8048518
inception010203 <inception010203@gmail.com>
2024-10-09 14:49:45 +0900

    Feat: 센서상태에 따라 다른 색상의 light 출력하도록 동적 클래스 설정(S11P21A202-878)
    Former-commit-id: cfa7430e4465cb14f12b7eb68339682230395a4c

--- client/src/app/home/_component/_sensor/sensor.tsx
+++ client/src/app/home/_component/_sensor/sensor.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b04ff3bb7b146e86beaa6a29a19f61223cdc04513d702bb579c8ce219845c274
-size 2734
+oid sha256:d7a66ba73f5e4a88bd7973a1e4f7790f6f4ee9b7dac34018dac1548753bc0034
+size 2875
