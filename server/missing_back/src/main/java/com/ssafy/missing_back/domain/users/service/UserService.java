e07744330e6cd85005a93de7d048e53085d881bc
LeeSpect <wewqwew153@gmail.com>
2024-10-09 13:37:08 +0900

    Feat: UserService에 지인 위험 레벨 정보를 리스트로 반환하는 메서드 구현(S11P21A202-644)
    Former-commit-id: 67235c97e7cc7eb5e1424584cc7b23b14f5e1600

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserService.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/users/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d53b92285e39a6bfe16be78e4dc45be8c44cd8a1569e82a4101d3b021185e95
-size 904
+oid sha256:29b5b2d6cabe3af1da9cd235b578a89363f1cfa61f23f45737ed5a0d59b1e7fd
+size 922
