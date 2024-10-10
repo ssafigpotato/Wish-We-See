42ea39e56289577319e48a8716ea024cd92b8982
jwun95 <jwun95@naver.com>
2024-10-11 02:29:33 +0900

    Fix: 버그 수정
    Former-commit-id: 21c2537a561723051365dffe4b5e6fefd9be1239

--- client/src/hooks/usePermissions.ts
+++ client/src/hooks/usePermissions.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7da1e70a653d49dff615e94eab37b7821c97ef2492ca88df30d454f4eb82e93c
-size 6091
+oid sha256:5dfde1cd9503158ef0067cba1f15106112aba7f747296f41c2dce6b09e31a61b
+size 6240
