8e817e31755a64f13b1b145a778c2c7087c133cb
inception010203 <inception010203@gmail.com>
2024-10-10 12:46:03 +0900

    Feat: 마이페이지 기본 정보 API 연결(S11P21A202-705)
    Former-commit-id: ba66f3a6580c6f09c71020e4173aee33db61da4b

--- client/src/store/useSafeZoneStore.tsx
+++ client/src/store/useSafeZoneStore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4454716bc36db4f6836f2707b389b940f75bf1a3f4d46ac9a6e98181e1566626
-size 2025
+oid sha256:c943dc7addeab9532b4be0016bca32fce3bb5ef240dbfad65e7ac603b75d6991
+size 2058
