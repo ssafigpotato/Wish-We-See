16cc0d879444c96ffb0a81efe1c7dc0df9c3ac44
inception010203 <inception010203@gmail.com>
2024-10-10 14:47:01 +0900

    Feat: 메인페이지에 isSafeZone변수 상태에 따라 안전유의지역 표시(S11P21A202-914)
    Former-commit-id: 8dff83d9c212170796246aa438f7e33d3a0a00eb

--- client/src/app/home/_component/_missinglist/missinglistfetch.tsx
+++ client/src/app/home/_component/_missinglist/missinglistfetch.tsx
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f5e8d5d1bf42c655de8686215f74e6982a42876a777596bd7fbc193fd7c75811
-size 474
+import axiosInstance from "@/app/api/axiosInstance";
+
+// 실종자 목록을 가져오는 함수
+export const fetchMissing = async () => {
+  try {
+    const response = await axiosInstance.get("/missing-persons/all"); // 실종자 목록
+    console.log(response);
+    return response.data; // 데이터 반환
+  } catch (error) {
+    console.error("실종자 데이터를 가져오는 중 오류 발생:", error);
+    throw error; // 에러를 호출자에게 전달
+  }
+};
