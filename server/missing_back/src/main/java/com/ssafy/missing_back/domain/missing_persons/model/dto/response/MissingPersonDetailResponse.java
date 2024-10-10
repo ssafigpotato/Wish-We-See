e2008eacf2da3d4e55733cfa6e3c9413539a6a39
LeeSpect <wewqwew153@gmail.com>
2024-10-10 14:03:53 +0900

    Feat: Additional Info 업데이트 할 때, 중복저장되지 않도록 로직 수정, 지인 상태 정보 조회 때 situation_id도 함꼐 전달, 실종자 목록 조회 시, situation_id도 함께 전달
    Former-commit-id: fb49d07daf8957e5a28843340e369049612bb56c

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/model/dto/response/MissingPersonDetailResponse.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/model/dto/response/MissingPersonDetailResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27d80c6a12cc0a4d8d54b99429f0f8a33ebdd5a7738a69093afc8d99c1154c93
-size 2268
+oid sha256:c19879be4c065bd7865a55e64593a7b64d4a992ac2cb4ea7068e1f4fd3163efe
+size 2374
