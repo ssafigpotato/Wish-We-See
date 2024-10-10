dd63d2176f7a797096031d71d4fa630b978331e1
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-10 10:44:08 +0900

    Fix: 위시 로고로 교체
    Former-commit-id: 622d796527127226f23cd93779ea2de7fc298819

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91b69a2f2d93d83ff9c1ea34b563dac2045f4288862ccda5d9cbd7c3672d760f
-size 699
+oid sha256:f6d9395fbc9ad74e478c2af7ee1f505b32a1b6561d49685dd49f4e4b6d6e1fa0
+size 701
