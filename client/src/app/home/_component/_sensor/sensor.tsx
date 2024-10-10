ac2c30e7003ef9f6ebb3abe9a74362ce37a4322a
jwun95 <jwun95@naver.com>
2024-10-11 08:15:10 +0900

    Fix: 로그인 버그 수정
    Former-commit-id: 672d12d8ed512725b73efde3166968535ba24c0a

--- client/src/app/home/_component/_sensor/sensor.tsx
+++ client/src/app/home/_component/_sensor/sensor.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cb85326ce68bcba35c10df3919c17944856c8d536519e5733f9f105fb2e27d2
-size 3161
+oid sha256:17dbbc3c8227ec36829fa6380ab6aa0c8f3b26fe90ac47f2ef042be20c8712b9
+size 3167
--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:144a0418587a3aa9308fc4568179a34a08434e09eeb2c682c5f76b8cb8eedf4d
-size 2834
+oid sha256:be030ca7e5891eb881b231ada6fa8251670b3c4a1aaa90e93db57a63f69af7a1
+size 2980
