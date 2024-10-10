77e09e5670ea57b215627e5d6eb8c4a2b5c4c5e8
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-10 14:22:43 +0900

    Feat: 추가 정보 페이지 서버 연동
    Former-commit-id: 1eb991b90c46a3cfa5bccb319d30cb01fe7f9d68

--- client/src/app/friend/_component/PhotoUpload/PhotoUpload.tsx
+++ client/src/app/friend/_component/PhotoUpload/PhotoUpload.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5f29d239641c7bd2519654adf334e87daf0c541a7013c025ac7af5b0e751874
-size 2135
+oid sha256:0d68f9eb60b729be26246d1516f58105fb53478d34f491da36d02d9e2d351ce2
+size 1497
