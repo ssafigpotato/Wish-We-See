75403cf026e8f1bbd8ced4a9dadffa6e24c5a745
inception010203 <inception010203@gmail.com>
2024-10-10 19:36:29 +0900

    Feat: 인상착의 등록시 마이페이지로 가도록 router.push 설정
    Former-commit-id: 8913683fc4e1cd24188582794062cdaa9c8a207a

--- client/src/app/mypage/modify/clothes/page.tsx
+++ client/src/app/mypage/modify/clothes/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1b397b75e36581562b5c2f8cd96c24714e6449f03aaee1fd1992acbc9cf8887
-size 5345
+oid sha256:71417fe2c8699d6af338e7bae1037850a219bf491ccd6d5214b3f28b50dae92a
+size 5473
