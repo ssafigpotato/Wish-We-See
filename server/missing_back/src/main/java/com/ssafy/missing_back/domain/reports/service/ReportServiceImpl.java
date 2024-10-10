7291722b3e6ebbc8fa44e30eaf4102d91cfb76c9
LeeSpect <wewqwew153@gmail.com>
2024-10-10 18:10:31 +0900

    Refactor: bodyScore 수정
    Former-commit-id: f59a482b76c729c564823ee1fdf0cf272413d972

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/service/ReportServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/reports/service/ReportServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a59449b0da27419ecc405703a2ba9d61192c083ccef13c142f90e61912f08d2c
-size 7470
+oid sha256:9573ca8c8b0a2764545391b4ec44fcd2aaf21398113de7ccbdbada71486eeec3
+size 7376
