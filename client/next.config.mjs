4460f972b5d6825ba9ad636c577ab9306ccb6bcb
jwun95 <jwun95@naver.com>
2024-10-10 09:21:44 +0900

    Fix: 제보 카메라 기능 Drag 버그 수정
    Former-commit-id: 3ec573f43aa980776f362ce430a0fab5dc327e61

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31b4caba9533b50db6b87b6b3100bc23c6f3804497cde3092badeff972038f70
-size 713
+oid sha256:c1f732ceba5968fa967ded3ec76b90f33f0662fb0cbabefa5d0161316cb2ab37
+size 844
