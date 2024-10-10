1e1c955375d953c7d59a78859e0ff32af264d26e
dongyeon <uuas5866@naver.com>
2024-10-10 15:08:38 +0900

    Fix: endpoint url 설정 수정
    Former-commit-id: e631af72f7685e5f040961c4154a5fafc00250c7

--- server/missing_back/src/main/java/com/ssafy/missing_back/global/config/SecurityConfig.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d68d2229a9092ca98e2c174b399c63cf102b724fc390f9a3c92da8370135565a
-size 3457
+oid sha256:d398d072febffa7e1f9b8e93033a462bd088e564b55da8dd07660dfd9be7be25
+size 3320
---
+++ server/missing_back/src/main/java/com/ssafy/missing_back/global/config/WebConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:538c6739bb6f0d2cb939f89296f33447f58125930de13e4ca09a1f9f6664d359
+size 460
