b955573a93da15a5fb8fe73ad6783e7827c88223
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-11 04:07:47 +0900

    Fix: [id] params 연동 수정
    Former-commit-id: fc9a36249a5bac0abbe9d2f237adf31e11ddbc2a

--- client/src/app/report/[id]/page.tsx
+++ client/src/app/report/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7500b4b578a261bf6dc377b7e8aeb5ab72022d157c1013eb29d67c28156a8bb
-size 2235
+oid sha256:6afbdf7e5f5497c960d239728397d7b1fc461625871ddfadf3887d95538c9463
+size 2344
