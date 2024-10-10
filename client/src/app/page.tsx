6ab87a951edcf6cdf067b514392a724baa8dca83
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-10 22:02:08 +0900

    Chore: next/image 속성 변경
    Former-commit-id: 09f850b8badb09470d3a92d3112faad7c11ff770

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,21 +1,3 @@
-"use client";
-
-import styles from "./page.module.scss";
-import Main from "./home/page";
-import { usePermissions } from "@/hooks/usePermissions";
-import Header from "@/components/Header/Header"
-import HeaderArea from "@/components/Header/HeaderArea";
-
-export default function App() {
-
-  usePermissions();
-  return (
-    <>
-      <HeaderArea />
-      <div className={styles.test}>
-        <Header />
-        <Main></Main>
-      </div>
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5496698a1745c9ee3ce1d96ab8ea5e8b8811a7615fd2510388fa232d8dbfe242
+size 448
