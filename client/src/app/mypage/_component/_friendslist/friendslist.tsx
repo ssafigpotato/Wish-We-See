dc48a0dee2edaafbaad73d8a271589de32173e72
inception010203 <inception010203@gmail.com>
2024-10-10 12:58:01 +0900

    Feat: 마이페이지 지인정보 API 연결(S11P21A202-705)
    Former-commit-id: c5479f4ab69335c04ea5387f20dcd1e346b12439

--- client/src/app/mypage/_component/_friendslist/friendslist.tsx
+++ client/src/app/mypage/_component/_friendslist/friendslist.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5f93f3364910f1d6f26a817c338352c4e00d5c99d71835340ba863ced6e641c
-size 876
+oid sha256:cfb2713abcb5ee20b193feafecc9fa3094daa7b4cc2f73c2f5c13eeefa429563
+size 927
