7bbbdd49d09ff426beb5b74aa1f7fb92a2799b4b
inception010203 <inception010203@gmail.com>
2024-10-10 15:26:00 +0900

    Fix: useSafezoneStore에 partialize로 isSafeZone변수만 저장(S11P21A202-914)
    Former-commit-id: fdb0e393de48e3f9c287d5a74ec3cd7d0e7192cd

--- client/src/store/useSafeZoneStore.tsx
+++ client/src/store/useSafeZoneStore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fad1b5f2f0d74a170a5a6786358608f210be3bf010437a1a234370c73bcc707
-size 3988
+oid sha256:b5ae4a3905059ed2c8548f5af3ca86c1c87db7bab085025445754141f75424b2
+size 4077
