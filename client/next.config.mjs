2caa954ba07f0e17cecd4c29e4287d6ef62b2bcf
tjdgus2308 <tjdgus2308@gmail.com>
2024-10-10 21:32:54 +0900

    Chore: next/image 설정 변경
    Former-commit-id: 5c7061e334f2051718243e68d8e17ffafe4ef185

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d064d40da1e47f98b3103ae2ae674c83f55c21a8350643453e29b36bb2ff7fdb
-size 1082
+oid sha256:6d2c95c70d77bc09e5d3fd7d1252026672e0340870c960f2b294d5e7a49e2d10
+size 1086
--- client/src/app/friend/_component/FriendReportListFetch.tsx
+++ client/src/app/friend/_component/FriendReportListFetch.tsx
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:06885e2a4fcfcc4098c74ace7f16e934ed3f7d9d851998526fbd8759715e44d2
-size 407
+import axiosInstance from "@/app/api/axiosInstance";
+
+const fetchFriendReportList = async (situation_id: number) => {
+  try {
+    const response = await axiosInstance.get(`/reports/${situation_id}`);
+    console.log("서버 응답:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("GET 요청 오류:", error);
+    throw error;
+  }
+};
+
+export default fetchFriendReportList;
