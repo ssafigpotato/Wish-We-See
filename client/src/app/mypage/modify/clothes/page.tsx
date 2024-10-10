5481ce058ffe8e35fd7fe325f6b5430e8ab340bf
inception010203 <inception010203@gmail.com>
2024-10-10 15:05:16 +0900

    Fix: 인상착의 등록페이지의 뒤로가기버튼을 link로 변경(S11P21A202-914)
    Former-commit-id: 2233c363c69fc9d36e46e467928c588998c15e6d

--- client/src/app/mypage/modify/clothes/page.tsx
+++ client/src/app/mypage/modify/clothes/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86546947501ec6025f8e0da3adf11c4cf4cd587be3810a9e7642a2fcaedff61d
-size 5184
+oid sha256:f1b397b75e36581562b5c2f8cd96c24714e6449f03aaee1fd1992acbc9cf8887
+size 5345
