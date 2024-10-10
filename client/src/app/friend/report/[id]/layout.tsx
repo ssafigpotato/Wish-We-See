8f8d584928b0de79b39590dfbabdbf3d09e1153f
inception010203 <inception010203@gmail.com>
2024-10-10 10:56:32 +0900

    Feat: 우범지역진입여부를 확인하기 위해 useSafezoneStore생성(S11P21A202-905)
    Former-commit-id: 2b52f7d9a935041d85f5c8b3415034f723e9fbda

--- client/src/app/friend/report/[id]/layout.tsx
+++ client/src/app/friend/report/[id]/layout.tsx
@@ -1,17 +1,3 @@
-import FreindReportList from "@/app/friend/report/[id]/page";
-import Header from "@/components/Header/Header";
-
-import styles from "@/app/friend/report/[id]/page.module.scss";
-import HeaderArea from "@/components/Header/HeaderArea";
-
-export default function FreindReportListPage() {
-  return (
-    <>
-      <HeaderArea />
-      <div className={styles["container"]}>
-        <Header />
-        <FreindReportList/>
-      </div>
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:99e86d461d071629b37cb14b79de141f48642216dd791a33cea8501c8fc84ca2
+size 441
---
+++ client/src/store/useSafeZoneStore.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df13b9555a6c8a0fec51e64f4105ea8e45f36ebfe804df01f303c3b52b9daca5
+size 561
