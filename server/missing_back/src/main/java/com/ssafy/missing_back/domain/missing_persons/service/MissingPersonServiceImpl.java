fd97ddc2810203ff57935ba35e5da07195019177
LeeSpect <wewqwew153@gmail.com>
2024-10-10 12:40:38 +0900

    Feat: 알람 클릭하면 앱으로 이동하도록 url 필드 추가
    Former-commit-id: 7df3964a847adbf758ec198af71d184ff630193b

--- server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonServiceImpl.java
+++ server/missing_back/src/main/java/com/ssafy/missing_back/domain/missing_persons/service/MissingPersonServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b4934cd27b0c623389f6addf30325557868cbda777dc9a8e4ecb8416465b24e
-size 5151
+oid sha256:af218a5eebd90bd6e49a6d7e37e49bc15fb7c5dbd6efdf04502194bd9b472bdd
+size 5195
