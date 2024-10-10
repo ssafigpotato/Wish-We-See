96f45bd084aaa8638d13ce634f7fae52185c78f1
inception010203 <inception010203@gmail.com>
2024-10-10 14:27:36 +0900

    Feat: user_additional_info가 존재하면 isSafezZone이 false가 되도록 로직 추가(S11P21A202-914)
    Former-commit-id: fce8693651d7f13dcf782294069f4ca3c77334a6

--- client/src/store/useSafeZoneStore.tsx
+++ client/src/store/useSafeZoneStore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30a1f215739c078aece212dda4d716b04dec3ac3c3559f990dd2e08019d203aa
-size 3792
+oid sha256:2fad1b5f2f0d74a170a5a6786358608f210be3bf010437a1a234370c73bcc707
+size 3988
