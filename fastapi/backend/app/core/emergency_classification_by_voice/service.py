ba340d32c6fa6f2ab8e3603afcfadb52eae3d82a
hybrid417 <hybrid417@naver.com>
2024-10-11 14:00:45 +0900

    Refactor: 음성 로직 리팩토링
    Former-commit-id: d3c66b4bd0983becdedc2bbfa9b0599d83ea0a0b

--- fastapi/backend/app/core/emergency_classification_by_voice/service.py
+++ fastapi/backend/app/core/emergency_classification_by_voice/service.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06825a8902c03f4657c8475801a6803ddb13ef8f736677083c8f224d50a65d29
-size 1313
+oid sha256:0290c7afc2723b049a6d4b9b600cf189b40139cabdef1cd4a69f181b79ee0670
+size 1341
