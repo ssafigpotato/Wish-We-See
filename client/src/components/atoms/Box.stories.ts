fc7f24849e3b94035ed13f4ca8b704d8a3e5967d
inception010203 <inception010203@gmail.com>
2024-10-17 23:16:33 +0900

    Feat: 스토리북 파일 생성 (S11P21A202-938)
    Former-commit-id: eb4fd97d64556ce48ba22d89063117050de0c8a9

---
+++ client/src/components/atoms/Box.stories.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f7561411afb39991b8c23ce9fe1fa74f4a8d796fd742577e74b2ccf33f293ed
+size 1433
---
+++ client/src/components/atoms/Box.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:00eee75e2ac5d5677dc5f60e360ba37ef4689ee8066efb80933447da05937dc5
+size 941
